883c8f6fa811e789c02e3c91ca3b62f7d2cd8b51
ALaN <digitalforce.ac@gmail.com>
2023-05-01 01:29:39 -0600

    Create persona added. yey

--- src/resolvers/index.ts
+++ src/resolvers/index.ts
@@ -1,8 +1,13 @@
 import { personas } from './personas';
 import { personasEdgy } from './personasEdgy';
+import { AddPersona } from './AddPersona'
+
 export const resolvers = {
   Query: {
     personas,
     personasEdgy
+  },
+  Mutation: {
+    AddPersona
   }
 };
